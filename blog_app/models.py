aede9f5d85a024cac80b53668f5b4740abe5333e
Андросов Петр <piter2074@yandex.com>
2020-01-23 07:32:33 +0300

    Added users ratings

--- blog_app/models.py
+++ blog_app/models.py
@@ -4,7 +4,7 @@ from tinymce.models import HTMLField
 
 
 class Post(models.Model):
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='posts')
     name = models.CharField(max_length=128)
     description = models.CharField(max_length=512)
     text = models.CharField(max_length=100000)
@@ -18,3 +18,14 @@ class Comment(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     text = models.CharField(max_length=512)
     post_id = models.IntegerField()
+
+
+def user_rating(self):
+    rating = 0
+    posts = self.posts.all()
+    for each in posts:
+        rating += each.rating
+    return rating
+
+
+User.add_to_class('user_rating', user_rating)
